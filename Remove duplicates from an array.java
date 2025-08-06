2963658925fab1b444bf47ae6c0285bafb81abeb
Priya Bhattacharya <priyabhattacharya2005@gmail.com>
2025-08-06 17:35:40 +0530

    Create Remove duplicates from an array.java

---
+++ Remove duplicates from an array.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+
+        for(int i=0;i<n;i++) {
+            arr[i]=sc.nextInt();
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i=0;i<n;i++) {
+            if(!set.contains(arr[i])) {
+                System.out.print(arr[i] + " ");
+                set.add(arr[i]);
+            }
+        }
+    }
+}
