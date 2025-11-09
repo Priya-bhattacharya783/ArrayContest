1c97098eced6d13d6ecf1ba16a89ee9222fe81b5
Priya Bhattacharya <priyabhattacharya2005@gmail.com>
2025-11-09 21:49:40 +0530

    Update Find minimum number of merge operations to make an array palindrome.java

--- Find minimum number of merge operations to make an array palindrome.java
+++ Find minimum number of merge operations to make an array palindrome.java
@@ -1,3 +1,17 @@
+Given an array of positive integers. 
+We need to make the given array a 'Palindrome'. 
+The only allowed operation is"merging" (of two adjacent elements). Merging two adjacent elements means replacing them with their sum. 
+The task is to find the minimum number of merge operations required to make the given array a 'Palindrome'.
+
+example :
+Input: arr[] = {1, 4, 5, 9, 1}
+i=0, j=4, arr[0]=1, arr[4]=1. arr[i] == arr[j]. i=1, j=3.
+i=1, j=3, arr[1]=4, arr[3]=9. arr[i] < arr[j]. Merge arr[1] with arr[2]. arr becomes {1, 9, 5, 9, 1} (conceptually, arr[2] is now 5+4=9). i=2, ans=1.
+i=2, j=3, arr[2]=9, arr[3]=9. arr[i] == arr[j]. i=3, j=2.
+i > j. Loop terminates.
+Output: ans = 1
+============================================================================================================================================================
+
 import java.io.*;
 import java.util.*;
 
